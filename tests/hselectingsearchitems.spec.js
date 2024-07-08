30e53218fa916e9f1c9ca54b983cbc127ca69387
vivek-tftus <singh.vivek@tftus.com>
2024-07-08 19:01:09 +0530

    adding some files

---
+++ tests/hselectingsearchitems.spec.js
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+import exp from "constants";
+import { sign } from "crypto";
+const{Dashboard} = require('../PageObjects/Dashboard')
+
+
+test.only("selecting search items and validating it ", async ({page})=>
+    {
+        //selecting 1 recomended search result
+        const dashboard = new Dashboard(page)
+        await dashboard.goTO()
+        await dashboard.SearchBox()
+
+
+        //Succesfully searched and validated the result
+        
+
+        
+
+
+
+
+
+    })
